77b308173be4b9a97829bf2098c56b82cf38c801
Ella van Durpe <ella@vandurpe.com>
2020-01-21 20:23:53 +0100

    Include multi selection nodes

--- packages/components/src/popover/utils.js
+++ packages/components/src/popover/utils.js
@@ -126,27 +126,13 @@ export function computePopoverYAxisPosition( anchorRect, contentSize, yAxis, cor
 	const { height } = contentSize;
 
 	if ( sticky ) {
-		let topEl = anchorRef;
-		let bottomEl = anchorRef;
-
-		if ( typeof sticky === 'string' ) {
-			const elements = document.querySelectorAll( sticky );
-
-			if ( elements.length ) {
-				topEl = elements[ 0 ];
-				bottomEl = elements[ elements.length - 1 ];
-			}
-		}
-
-		const scrollContainerEl = getScrollContainer( topEl ) || document.body;
+		const scrollContainerEl = getScrollContainer( anchorRef ) || document.body;
 		const scrollRect = scrollContainerEl.getBoundingClientRect();
-		const topRect = topEl.getBoundingClientRect();
-		const bottomRect = bottomEl.getBoundingClientRect();
 
-		if ( topRect.top - height <= scrollRect.top ) {
+		if ( anchorRect.top - height <= scrollRect.top ) {
 			return {
 				yAxis,
-				popoverTop: Math.min( bottomRect.bottom, scrollRect.top + height ),
+				popoverTop: Math.min( anchorRect.bottom, scrollRect.top + height ),
 			};
 		}
 	}
